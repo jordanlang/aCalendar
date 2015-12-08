2f3081929cd11a8b0cb1df404241cb8597bea520
romainhry <romainhry@gmail.com>
2015-12-08 16:21:11 +0100

    models activite et evaluation :-*

---
+++ models/evaluation.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once 'base.php';
+
+class Agenda extends Model_Base
+{
+	private $_idUtilisateur;
+
+	private $_idActivite;
+
+	private $_note;
+
+	private $_dateEval;
+
+	public function __construct($idUtilisateur, $idActivite, $note, $dateEval) {
+
+		$this->set_idUtilisateur($idUtilisateur);
+		$this->set_idActivite($idActivite);
+		$this->set_note($note);
+		$this->set_dateEval($dateEval);
+  }
+
+	//get
+
+	public function idUtilisateur() {
+		return $this->_idUtilisateur;
+	}
+
+	public function idActivite() {
+		return $this->_idActivite;
+	}
+
+	public function note() {
+		return $this->_note;
+	}
+
+	public function dateEval() {
+		return $this->_dateEval;
+	}
+
+	//set
+
+	private function set_idUtilisateur($v) {
+		$this->_idUtilisateur = (int) $v;
+	}
+
+	private function set_idActivite($v) {
+		$this->_idActivite = (int) $v;
+	}
+
+	private function set_note($v) {
+		$this->_note = (int)($v);
+	}
+
+	private function set_dateEval($v) {
+		$this->_dateEval = (int)($v);
+	}
+
+
+	public function add() {
+		if(!is_null($this->_id)) {
+			$q = self::$_db->prepare('INSERT INTO ACTIVITE (idUtilisateur, idActivite,
+        note, dateEval) VALUES (:idUtilisateur,:idActivite,:note, :dateEval)');
+      $q->bindValue(':idUtilisateur', $this->_idUtilisateur, PDO::PARAM_INT);
+      $q->bindValue(':idActivite', $this->_idActivite, PDO::PARAM_INT);
+      $q->bindValue(':note', $this->_note, PDO::PARAM_INT);
+			$q->bindValue(':dateEval', $this->_dateEval, PDO::PARAM_STRING);
+			$q->execute();
+		}
+	}
+
+	public function delete()
+	{
+		if(!is_null($this->_id)) {
+			$q = self::$_db->prepare('DELETE FROM ACTIVITE WHERE idActivite = :id');
+			$q->bindValue(':id', $this->_idActivite);
+			$q->execute();
+			$this->_id = null;
+		}
+	}
+
+	public static function get_by_idActivite($idActivite) {
+		$s = self::$_db->prepare('SELECT * FROM ACTIVITE
+      where idActivite = :id');
+		$s->bindValue(':id', $idActivite, PDO::PARAM_INT);
+		$s->execute();
+    $evaluations = array();
+		while($data = $s->fetch(PDO::FETCH_ASSOC)) {
+			$evaluations[] = new Evaluation(
+        $data['idUtilisateur'], $data['idActivite'],
+        $data['note'], $data['dateEval']
+      );
+		}
+		return $evaluations;
+	}
+
+	public static function get_by_idUtilisateur($id) {
+		$s = self::$_db->prepare('SELECT * FROM ACTIVITE where idUtilisateur=:id');
+		$s->bindValue(':id', $id, PDO::PARAM_INT);
+		$s->execute();
+    $evaluations = array();
+		while($data = $s->fetch(PDO::FETCH_ASSOC)) {
+			$evaluations[] = new Evaluation(
+        $data['idUtilisateur'], $data['idActivite'],
+        $data['note'], $data['dateEval']
+      );
+		}
+		return $evaluations;
+	}
+
+  public static function get_by_ids($idUtilisateur,$idActivite) {
+		$s = self::$_db->prepare('SELECT * FROM ACTIVITE
+      where idActivite = :idActivite AND idUtilisateur =:idUtilisateur');
+		$s->bindValue(':idActivite', $idActivite, PDO::PARAM_INT);
+    $s->bindValue(':idUtilisateur', $idUtilisateur, PDO::PARAM_INT);
+    $s->execute();
+    $evaluations = array();
+		$data = $s->fetch(PDO::FETCH_ASSOC);
+    if($data) {
+      return new Evaluation(
+        $data['idUtilisateur'], $data['idActivite'],
+        $data['note'], $data['dateEval']
+      );
+    }
+		return null;
+	}
+}
